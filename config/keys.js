b6d2d1b181899f9e31812e9b6d9df967ba6385b4
59DownHill <sushen1990@163.com>
2019-09-25 02:12:07 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,1 @@
-module.exports = {
-  // mongoURI:
-  //   'mongodb://nodevueelement:test1234@ds223756.mlab.com:23756/node-vue-element',
-  mongoURI:
-    'mongodb://aly_root:d456_FJ35LLL@39.97.33.102:27899/admin',
-  secretOrKey: 'secret'
-};
+
