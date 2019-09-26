33514cdc17bf7be63fa1ec627f9e12a69148bea8
lap1 <lap1>
2019-09-26 18:45:00 +0800

    client_test

---
+++ client_test/src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="index">
+		<div class="container">
+			<h1 class="title">米修在线</h1>
+			<p class="lead"> 专注于线上教育, 用心做课程, 用心做服务! </p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'index',
+		components: {}
+	}
+</script>
+
+<style>
+</style>
